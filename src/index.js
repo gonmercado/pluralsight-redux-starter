6eb8119512ef9f03c20c8823b617db3c5c31fcc7
Gonzalo Mercado <gonzalo.mercado@sovos.com>
2017-05-18 11:41:51 -0300

    Adding store, actions and reducers for course list

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { render } from 'react-dom';
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { loadCourses } from './actions/CourseActions';
 import App from './components/App';
 
 import './styles/styles.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 const Sgames = () => (
   <Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import Thunk from 'redux-thunk';
 
 export default function configureStore(initialState){
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariant())
+    applyMiddleware(Thunk, reduxImmutableStateInvariant())
   );
 }
